a4e04ae7810c7ef93777954d3ff71702c4b88c16
adrnl1ne <jako7096@stud.kea.dk>
2022-11-12 16:36:11 +0100

    yeeeee

---
+++ src/main/java/com/twitctime/plzletmepasstheexams/PlzLetMePassTheExamsApplication.java
@@ -0,0 +1,13 @@
+package com.twitctime.plzletmepasstheexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlzLetMePassTheExamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlzLetMePassTheExamsApplication.class, args);
+    }
+
+}
