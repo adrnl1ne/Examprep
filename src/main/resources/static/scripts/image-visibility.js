28fbe2ed2d2fafd440037039b2271888eb56e86f
Marcus <marc303i@stud.kea.dk>
2022-11-18 20:41:54 +0100

    Nu vil der komme billeder, på det første spørgsmål, når man holder musen over et ord

---
+++ src/main/resources/static/scripts/image-visibility.js
@@ -0,0 +1,76 @@
+$(function () {
+    $("#t-1-1-0").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-1").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-2").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-3").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-3").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-4").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-4").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-5").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-5").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-6").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-6").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-7").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-7").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-8").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-8").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-1-9").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-9").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#qs-1").hover(function () {
+        $(".container .image").css("visibility", "hidden");
+        $(".original-image").css("visibility", "visible")
+    })
+});
