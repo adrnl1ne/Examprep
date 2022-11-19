5b440af3a84bb5a95ed1dc89cd3b6a525c7a4eb7
Marcus <marc303i@stud.kea.dk>
2022-11-19 20:47:55 +0100

    Nu vil der komme billeder, på alle spørgsmål, når man holder musen over et ord Dog ikke alle steder er der billeder

--- src/main/resources/static/scripts/image-visibility.js
+++ src/main/resources/static/scripts/image-visibility.js
@@ -1,5 +1,7 @@
 $(function () {
     $("#t-1-1-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-0").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-0").css("visibility", "visible");
     })
@@ -7,6 +9,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-1").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-1").css("visibility", "visible");
     })
@@ -14,6 +18,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-2").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-2").css("visibility", "visible");
     })
@@ -21,6 +27,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-3").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-3").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-3").css("visibility", "visible");
     })
@@ -28,6 +36,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-4").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-4").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-4").css("visibility", "visible");
     })
@@ -35,6 +45,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-5").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-5").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-5").css("visibility", "visible");
     })
@@ -42,6 +54,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-6").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-6").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-6").css("visibility", "visible");
     })
@@ -49,6 +63,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-7").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-7").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-7").css("visibility", "visible");
     })
@@ -56,6 +72,8 @@ $(function () {
 
 $(function () {
     $("#t-1-1-8").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-8").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
         $("#i-1-1-8").css("visibility", "visible");
     })
@@ -63,13 +81,336 @@ $(function () {
 
 $(function () {
     $("#t-1-1-9").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-1-9").css("background-color", "lightgray");
         $(".container .image").css("visibility", "hidden");
-        $("#i-1-1-9").css("visibility", "visible");
+        let randomNum = Math.floor(Math.random() * 2) + 1;
+        if (randomNum === 1) {
+            $("#i-1-1-9").css("visibility", "visible");
+        } else {
+            $("#i-1-1-10").css("visibility", "visible");
+        }
     })
 });
 
 $(function () {
-    $("#qs-1").hover(function () {
+    $("#t-1-2-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-2-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-2-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-4-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-4-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-4-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-4-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-4-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-4-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-4-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-4-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-4-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-6-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-6-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-6-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-6-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-6-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-6-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-6-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-6-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-6-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-6-3").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-6-3").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-1-3").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-7-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-7-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-7-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-7-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-7-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-7-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-7-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-7-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-7-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-7-3").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-7-3").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-7-3").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-9-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-9-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-9-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-9-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-9-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-9-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-1-9-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-1-9-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-1-9-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-2-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-2-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-2-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-2-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-2-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-2-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-4-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-4-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-4-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-5-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-5-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-5-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-5-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-5-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-5-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-5-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-5-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-5-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-5-3").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-5-3").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-5-3").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-2-5-4").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-2-5-4").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-2-5-4").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-1-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-1-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-1-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-2-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-2-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-2-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-2-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-2-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-2-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-2-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-2-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-2-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-2-3").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-2-3").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-2-3").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-3-4-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-3-4-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-3-4-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-1-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-1-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-1-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-2-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-2-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-2-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-3-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-3-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-3-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-4-0").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-4-0").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-4-0").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-4-1").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-4-1").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-4-1").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $("#t-4-4-2").hover(function () {
+        $(".container h3").css("background-color", "transparent");
+        $("#t-4-4-2").css("background-color", "lightgray");
+        $(".container .image").css("visibility", "hidden");
+        $("#i-4-4-2").css("visibility", "visible");
+    })
+});
+
+$(function () {
+    $(".flex-container .container h2, .question").hover(function () {
+        $(".container h3").css("background-color", "transparent");
         $(".container .image").css("visibility", "hidden");
         $(".original-image").css("visibility", "visible")
     })
